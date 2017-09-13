ae4e6a54acc931e921d28b94efda6ee722ea3f18
James Randolph <james.randolph2016@gmail.com>
2017-09-13 16:36:57 +0000

    testing message

--- lib/Hotel.rb
+++ lib/Hotel.rb
@@ -3,3 +3,13 @@ class TopUSHotel::Hotel
   attr_accessor :name, :rank, :location, :url
 
   @@all = []
+
+  def initialize(name=nil, rank=nil, location=nil, url=nil)
+    @name = name
+    @rank = rank
+    @location = location
+    @url = url
+  end
+
+  #testing commit changes
+end
