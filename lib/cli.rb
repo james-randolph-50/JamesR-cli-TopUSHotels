8a89cd42721a77a5caab3fa8bce6cf6ae9afebef
James Randolph <james.randolph2016@gmail.com>
2017-09-14 22:34:03 +0000

    Actually subtracting by 1 now :)

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,7 +8,11 @@ class TopUSHotels::CLI
 
   def begin
     puts "Which hotels would you like to see? Please type: 1-5, 6-10, or 11-15"
-    input = gets.strip.to_i
+    input = gets.strip.to_i-1
+
+    show_hotel(input)
+    puts "Which hotel would you like more information on?"
+    input = gets.strip.to_i-1
 
   end
 
