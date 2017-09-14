d63b93bbc26eee140806394b10d97c5f9c75c522
James Randolph <james.randolph2016@gmail.com>
2017-09-14 16:46:55 +0000

    Defined init method with info from attr_accessors.

--- bin/TopUSHotels.rb
+++ bin/TopUSHotels.rb
@@ -3,4 +3,11 @@ require_relative '../lib/TopUSHotels'
   class TopUSHotel::Hotel
     attr_accessor :name, :rank, :location, :url
 
+    def initialize(name, rank, location, url)
+      @name = name
+      @rank = rank
+      @location = location
+      @url = url
+    end
+
   end
