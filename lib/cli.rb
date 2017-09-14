00a3a33ad3a859d81a585b219f0507e48f3648d9
James Randolph <james.randolph2016@gmail.com>
2017-09-14 22:24:50 +0000

    allows user to select a range of the top hotels. Takes user input and subtracts 1 to access the correct element in the array.

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,4 +4,12 @@ class TopUSHotels::CLI
     TopUSHotels::Scraper.new.list_hotels
     puts "Check out the Top Hotels in the U.S.!"
     begin
+  end
+
+  def begin
+    puts "Which hotels would you like to see? Please type: 1-5, 6-10, or 11-15"
+    input = gets.strip.to_i
+
+  end
+
 end
