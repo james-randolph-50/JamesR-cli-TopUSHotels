4bc622bf4423d081eedb6c22fbc7ae9fae2117a2
James Randolph <james.randolph2016@gmail.com>
2017-09-14 16:48:21 +0000

    defined slef.all method to return all instances of Hotels. Also added @@all class variable, equal to an empty array.

--- bin/TopUSHotels.rb
+++ bin/TopUSHotels.rb
@@ -3,11 +3,18 @@ require_relative '../lib/TopUSHotels'
   class TopUSHotel::Hotel
     attr_accessor :name, :rank, :location, :url
 
+    @@all = []
+
     def initialize(name, rank, location, url)
       @name = name
       @rank = rank
       @location = location
       @url = url
     end
+
+    def self.all
+      @@all
+    end
+
 
   end
